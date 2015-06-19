fcd2269e6733bc7c0800e7a18e967ad6da6f1176
Andrew Kelley <jakelley@Andrews-MacBook-Pro.local>
2015-06-19 16:58:36 -0400

    Testing backend map query generation
    Testing backend map query generation

--- app/controllers/openFDA.server.controller.js
+++ app/controllers/openFDA.server.controller.js
@@ -3,21 +3,37 @@
 var openFDAService = require('./../services/openfda.server.service');
 
 module.exports.queryOpenFDA = function(req,res){
-    var queryId = req.params.qId;
-    console.log("quId: " + queryId);
+    //var queryId = 1;//req.params.qId;
+    //console.log("quId: " + queryId);
 
     var currentQuery = {};
 
-    queries.forEach(function(query){
-      if(query.queryId === queryId){
-        currentQuery = query;
-      }
-    })
+    // queries.forEach(function(query){
+    //   if(query.queryId === queryId){
+    //     currentQuery = query;
+    //   }
+    // })
 
-    openFDAService.getData(currentQuery,function(error,data){
-      console.log('callback');
-      console.log(data);
+    var stateQueries = generateStateCountQueries();
+    var results = {};
+
+    stateQueries.forEach(function(entry){
+      openFDAService.getData(entry,function(error,data){
+        data = JSON.parse(data);
+        console.log(data.results[0].term + " : " + data.results[0].count);
+        results[data.results[0].term] = { filkey: 'H', count: data.results[0].count};
+        console.log(results);
+      });
     });
+
+    console.log('results');
+    console.log(results);
+
+    // openFDAService.getData(currentQuery,function(error,data){
+    //   console.log('callback');
+    //   console.log(data);
+    // });
+
     var result = "success";
     console.log("result: " + result);
     res.send(result);
@@ -37,10 +53,42 @@ module.exports.queryOpenFDA = function(req,res){
   }
 ]
 */
-
 function transfromData(data){
-  return data;
+  var result = {};
+  var state = data.results[0].term
+  result.push(
+    {
+       state:{
+        fillKey: 'H',
+        count: data.results[0].count
+      }
+    }
+  );
+  return result;
 };
+
+function generateStateCountQueries(){
+  var states = ['va','tx','ny'];
+  var stateQueries = [];
+  states.forEach(function(state){
+    stateQueries.push(
+      {
+        queryId: 1,
+        noun:'drug',
+        endpoint:'enforcement',
+        params:{
+          search:'distribution_pattern:"' + state + '"',
+          count:'distribution_pattern',
+          limit:0,
+          skip:0
+        }
+      }
+    );
+  });
+
+  //console.log(stateQueries);
+  return stateQueries;
+}
 
 var queries = [
   {
@@ -48,9 +96,9 @@ var queries = [
     noun:'drug',
     endpoint:'enforcement',
     params:{
-      search:'state:"VA"',
+      search:'distribution_pattern:"va"',
       count:'distribution_pattern',
-      limit:25,
+      limit:0,
       skip:0
     }
   }
