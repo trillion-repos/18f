ea189b85091ff6409552b5b9d9b8a76ffd9de68b
Andrew Kelley <jakelley@Andrews-MacBook-Pro.local>
2015-06-23 17:23:10 -0400

    Corrected errors in graphOfda service
    Corrected errors in graphOfda service and also the naming typo for
    queryOfda.server.service.js

--- app/services/graphOfda.server.service.js
+++ app/services/graphOfda.server.service.js
@@ -2,28 +2,31 @@
 
 var queryService = require("./queryOfda.server.sevice");
 
-modules.exports.graphRpy = function (callback){
+module.exports.graphRpy = function (callback){
 	var response = {};
-	var response.graphData = {};
-	var datasets = [{name:'drug', displayName="Drugs"},{name:'device', displayName="Devices"},{name:'food', displayName="Food"}];
-	
-	
+	response.graphData = {};
+	var datasets = [{name:'drug', displayName:"Drugs"},{name:'device', displayName:"Devices"},{name:'food', displayName:"Food"}];
+
+	console.log('in graph');
+
 	datasets.forEach(function(dataset){
 		var query = {
 			    queryId: 1,
 			    noun:dataset.name,
 			    endpoint:'enforcement',
 			    params:{
-			      search:'distribution_pattern:"va"',
-			      count:'distribution_pattern',
+			      search:'(distribution_pattern:VA+Virginia)+AND+(report_date:[2007-01-01+TO+2015+01+01])',
+			      count:'report_date',
 			      limit:1000, //if set to 0, it will default to 100 results
 			      skip:0
 			    }
 			  };
-		
-		queryService.getData(allTermQuery,function(error,data, query){
+
+				console.log(query);
+
+		queryService.getData(query,function(error,data, query){
 			completeQueries++;
-			
+
 			if(error){
 				console.error("ERROR: ", JSON.stringify(error), JSON.stringify(allTermQuery));
 				return;
@@ -37,11 +40,11 @@ modules.exports.graphRpy = function (callback){
 				console.log("No Results for: " + JSON.stringify(allTermQuery));
 				return;
 			}
-			
+
 			//console.log("RAW DATA: ", data);
-			
-			
-			
+
+
+
 			if (completeQueries == datasets.length){
 				response.graphData = {
 					    series: getDisplayNames(),
@@ -62,20 +65,20 @@ modules.exports.graphRpy = function (callback){
 				console.log('results: ' + JSON.stringify(response));
 				callback(null, response);
 			}
-			
+
 
 		});
-		
-		
+
+
 	});
-	
-	
+
+
 	function getDisplayNames(){
 		var displayNames = [];
 		datasets.forEach(function(dataset){
 			displayNames.push(dataset.displayName);
 		});
-		
+
 		return displayNames;
 	}
 };
