74a279e6d669ea7a74490a2a6a03d3019c7d6fca
Andrew Kelley <jakelley@Andrews-MacBook-Pro.local>
2015-06-26 16:06:41 -0400

    Added assertions for drug, food and device to map test suite
    Added assertions for drug, food and device to map test suite

--- app/spec/mapOfda.server.service.spec.js
+++ app/spec/mapOfda.server.service.spec.js
@@ -7,9 +7,17 @@ describe("Test Suite for maphOfda.server.service", function() {
 
   it("Spec for mapRps function", function(done) {
     mapService.mapRps(params,function( erro,response){
-        //expect there to be all 50 states with defined properties of fillKey,count, and label
+        //expect drug,food,and devices to be retunred
+        expect(response.mapData.drug).toBeDefined();
+        expect(response.mapData.food).toBeDefined();
+        expect(response.mapData.device).toBeDefined();
+
+        /* Drill down in one to make sure the data is correct, expect there
+         to be all 50 states with defined properties of fillKey,count,
+         and label */
         for(var state in states){
-          if(state != "nd" && state != "sd" ){//no data for SD or ND for some reson, exlcuding them from the test
+          //no data for SD or ND for some reson, exlcuding them from the test
+          if(state != "nd" && state != "sd" ){
             expect(response.mapData.drug[state]).toBeDefined();
             expect(response.mapData.drug[state].fillKey).toBeDefined();
             expect(response.mapData.drug[state].count).toBeDefined();
